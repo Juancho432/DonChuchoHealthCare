034ffe7eccf283d4b4df89e7354a867866b56ec3
Emmanuel Mora <emmanuel.mora388@pascualbravo.edu.co>
2025-10-27 17:07:23 -0500

    Instalacion de paquetes NuGet (MySqlData) y creacion de clases Cliente y CD_Cliente

---
+++ CapaDatos/CD_Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace CapaDatos
+{
+    public class CD_Cliente
+    {
+
+    }
+}
---
+++ CapaDatos/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Cliente
+    {
+        public string IdCliente { get; set; }           
+        public string TipoDocumento { get; set; }       // CC, CE, NIT, Pasaporte
+        public string Nombre { get; set; }
+        public string Apellidos { get; set; }
+        public DateTime? FechaNacimiento { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Correo { get; set; }
+        public string HistorialCrediticio { get; set; }
+        public DateTime FechaRegistro { get; set; }
+    }
+}
