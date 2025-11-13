8d573bb656f4c8d35fed75e2c2c4a89b4435bfcd
Juancho432 <juanmanueljaramillojaramillo4@gmail.com>
2025-11-13 15:58:26 -0500

    Asignacion de valores a campos en busqueda de clientes

--- DonChuchoHealthCare/Clientes.aspx.cs
+++ DonChuchoHealthCare/Clientes.aspx.cs
@@ -38,8 +38,8 @@ namespace DonChuchoHealthCare
         }
 
         public void btn_buscar_Click(object sender, EventArgs e)
-        {
-            // Simulación: validar que haya escrito un ID
+        { 
+
             if (string.IsNullOrWhiteSpace(txt_buscarId.Text))
             {
                 return; // no hace nada si no hay ID
@@ -47,17 +47,16 @@ namespace DonChuchoHealthCare
 
             Cliente? data = objCN.BuscarCliente(txt_buscarId.Text);
 
-            // Supongamos que encontró un cliente:
-            if (data != null)
+            if (data is Cliente cliente)
             {
-                txt_id_admin.Text = txt_buscarId.Text;
-                ddl_tipoDocumento_admin.SelectedValue = "CC";
-                txt_nombres_admin.Text = "Carlos";
-                txt_apellidos_admin.Text = "Pérez";
-                txt_fechaNacimiento_admin.Text = "1990-05-12";
-                txt_direccion_admin.Text = "Calle 45 #23-10";
-                txt_telefono_admin.Text = "3124567890";
-                txt_correo_admin.Text = "carlos.perez@example.com";
+                txt_id_admin.Text = cliente.id_cliente;
+                ddl_tipoDocumento_admin.SelectedIndex = ((int)cliente.tipo_documento);
+                txt_nombres_admin.Text = cliente.nombre;
+                txt_apellidos_admin.Text = cliente.apellidos;
+                txt_fechaNacimiento_admin.Text = cliente.fecha_nacimiento.Date.ToString();
+                txt_direccion_admin.Text = cliente.direccion;
+                txt_telefono_admin.Text = cliente.telefono;
+                txt_correo_admin.Text = cliente.correo;
 
                 ddl_tipoDocumento_admin.Enabled = true;
                 txt_id_admin.ReadOnly = false;
