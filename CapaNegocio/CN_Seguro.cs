c20c269ebfba0856c831a3aa5a7804ef0a5944f5
Juancho432 <juanmanueljaramillojaramillo4@gmail.com>
2025-11-20 18:58:01 -0500

    Añadida Funcionalidad de Registro de Seguros

--- CapaNegocio/CN_Seguro.cs
+++ CapaNegocio/CN_Seguro.cs
@@ -1,9 +1,23 @@
 ﻿
 using Entidades;
+using System.Data;
+using CapaDatos;
 
 namespace CapaNegocio
 {
     public class CN_Seguro
     {
+        private readonly CD_Seguro objCD = new CD_Seguro();
+
+        public DataTable ListarSeguros()
+        {
+            return objCD.ListarSeguros();
+        }
+
+        public bool InsertarSeguro(Seguro seguro)
+        {
+            objCD.InsertarSeguro(seguro);
+            return true; 
+        }
     }
 }
--- DonChuchoHealthCare/Seguros.aspx.cs
+++ DonChuchoHealthCare/Seguros.aspx.cs
@@ -1,21 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
+using CapaNegocio;
+using Entidades;
 
 namespace DonChuchoHealthCare
 {
     public partial class Seguros : Page
     {
+
+        private readonly CN_Seguro objCN = new CN_Seguro();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            gv_seguros.DataSource = objCN.ListarSeguros();
+            gv_seguros.DataBind();
         }
 
         protected void btn_guardar_Click(object sender, EventArgs e)
         {
-            // Lógica de inserción de nuevo seguro
+            Seguro data = new Seguro
+            {
+                nombre = txt_nombre.Text,
+                tipo_seguro = (Tipo_Seguro)ddl_tipo.SelectedIndex + 1,
+                cobertura = txt_cobertura.Text,
+                costo = decimal.Parse(txt_costo.Text),
+                duracion_meses = int.Parse(txt_duracion.Text),
+                id_aseguradora = int.Parse(ddl_aseguradora.SelectedValue),
+                estado = Estado_Seguro.Activo,
+                beneficios = txt_beneficios.Text,
+                exclusiones = txt_exclusiones.Text,
+                condiciones = txt_condiciones.Text
+            };
+
+            objCN.InsertarSeguro(data);
         }
 
         protected void btn_limpiar_Click(object sender, EventArgs e)
